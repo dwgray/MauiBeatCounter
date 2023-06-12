8d53e5436aa3f850eb1e14e96669790c0db0d6e5
David W. Gray <dwgray67@hotmail.com>
2023-06-12 09:51:41 -0700

    Add project files.

---
+++ MauiBeatCounter/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiBeatCounter;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
